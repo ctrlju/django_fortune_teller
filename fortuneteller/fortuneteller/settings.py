6ce0b2c474244615b709eeb39f17e5ce4068f110
ctrlju <julianaferreiralp@outlook.com>
2023-12-12 21:12:18 -0300

    Creating the homepage and configuring the path list

--- fortuneteller/fortuneteller/settings.py
+++ fortuneteller/fortuneteller/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'randomfortune.apps.RandomfortuneConfig',
 ]
 
 MIDDLEWARE = [
--- fortuneteller/randomfortune/views.py
+++ fortuneteller/randomfortune/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def fortune(request):
+    return render(request, "randomfortune/fortune.html")
